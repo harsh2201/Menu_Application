cf1c854de15c1b57e01afd2488999800904f09f6
Brij S Patel <brijpatel1823@gmail.com>
2019-05-11 21:44:11 +0530

    Auto Slider

--- app/src/main/java/com/hbteam/menuapplication/Activity/MainActivity.java
+++ app/src/main/java/com/hbteam/menuapplication/Activity/MainActivity.java
@@ -3,9 +3,13 @@ package com.hbteam.menuapplication.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
 import android.view.animation.OvershootInterpolator;
@@ -17,12 +21,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.hbteam.menuapplication.Adapter.ItemsAdapter;
+import com.hbteam.menuapplication.Class.ImageModel;
 import com.hbteam.menuapplication.Class.ItemClass;
+import com.hbteam.menuapplication.Class.SlidingImage_Adapter;
 import com.hbteam.menuapplication.R;
-
+import com.viewpagerindicator.CirclePageIndicator;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
 import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
@@ -34,6 +42,16 @@ public class MainActivity extends AppCompatActivity {
     List<ItemClass> facultyList;
     public Intent intent;
     public android.support.v7.widget.SearchView searchview;
+
+   //Auto SLider
+    private ArrayList<ImageModel> imageModelArrayList;
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    private static int NUM_PAGES = 0;
+
+    private int[] myImageList = new int[]{R.drawable.one, R.drawable.two,
+           R.drawable.three,R.drawable.four};
+
 
 
     //FireBase Content
@@ -59,128 +77,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Auto Slider
+        imageModelArrayList = new ArrayList<>();
+        imageModelArrayList = populateList();
+
+        init();
+
         rootRef = FirebaseDatabase.getInstance().getReference();
         demoRef = rootRef.child("New Products");
         old_demoRef = rootRef.child("Old Products");
+
 
 
         facultyList = new ArrayList<>();
         recyclerView = findViewById(R.id.recycler);
 
 
-        //Removed as it was working only in debuggin mode.
-        
 
        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
-        //Loading the data from firebase
-//        demoRef.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                int length= (int) dataSnapshot.getChildrenCount();
-//                for(int i=1;i<=length;i++){
-//                    String name = dataSnapshot.child(""+i).child("Drug Name").getValue(String.class);
-//                    Integer mrp = dataSnapshot.child(""+i).child("MRP").getValue(Integer.class);
-//                    String packing = dataSnapshot.child(""+i).child("Packing").getValue(String.class);
-//                    Integer quantity = dataSnapshot.child(""+i).child("Shipper Quantity").getValue(Integer.class);
-//                    //Toast.makeText(MainActivity.this,""+name,Toast.LENGTH_SHORT).show();
-//
-//
-//
-//                    new_name[new_counter]=name;
-//                    new_mrp[new_counter]=mrp;
-//                    new_packing[new_counter]=packing;
-//                    new_quantity[new_counter]=quantity;
-//
-//
-//                    /*facultyList.add(
-//                            new ItemClass(
-//                                    R.drawable.sample,
-//                                    name,
-//                                    packing
-//                            )
-//                    );*/
-//
-//                    new_counter++;
-//
-//                }
-//
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//            }
-//
-//
-//
-//
-//        });
-//
-//
-//        old_demoRef.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                int length= (int) dataSnapshot.getChildrenCount();
-//                for(int i=1;i<=length;i++){
-//                    String name = dataSnapshot.child(""+i).child("Drug Name").getValue(String.class);
-//                    Integer mrp = dataSnapshot.child(""+i).child("MRP").getValue(Integer.class);
-//                    String packing = dataSnapshot.child(""+i).child("Packing").getValue(String.class);
-//                    Integer quantity = dataSnapshot.child(""+i).child("Shipper Quantity").getValue(Integer.class);
-//                    //Toast.makeText(MainActivity.this,""+name,Toast.LENGTH_SHORT).show();
-//                    old_name[old_counter]=name;
-//                    old_mrp[old_counter]=mrp;
-//                    old_packing[old_counter]=packing;
-//                    old_quantity[old_counter]=quantity;
-//
-//                    /*facultyList.add(
-//                            new ItemClass(
-//                                    R.drawable.sample,
-//                                    name,
-//                                    packing
-//                            )
-//                    );*/
-//
-//                    old_counter++;
-//
-//
-//
-//
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//            }
-//
-//
-//
-//        });
-//
-//
         Context context=this;
         get_newproducts(context);
         get_oldproducts(context);
-        //Adding Data to th list:
-        //addList();
+    }
 
-//        Context con=recyclerView.getContext();
-//        LayoutAnimationController controller= AnimationUtils.loadLayoutAnimation(con,R.anim.lay_fall_down);
-//        adapter=new ItemsAdapter(this,facultyList);
-//
-////        ScaleInAnimationAdapter alphaAdapter = new ScaleInAnimationAdapter(adapter);
-//        AlphaInAnimationAdapter alphaAdapter2 = new AlphaInAnimationAdapter(adapter);
-//        ScaleInAnimationAdapter alphaAdapter = new ScaleInAnimationAdapter(alphaAdapter2);
-//        alphaAdapter.setDuration(1000);
-//        alphaAdapter.setInterpolator(new OvershootInterpolator());
-//        alphaAdapter.setFirstOnly(false);
-//        recyclerView.setAdapter(alphaAdapter);
-////        recyclerView.setAdapter(adapter);
-//        recyclerView.setLayoutAnimation(controller);
-//        recyclerView.scheduleLayoutAnimation();
-//        onClickList();
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mymenu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     public void addList(){
@@ -212,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
 //        ScaleInAnimationAdapter alphaAdapter = new ScaleInAnimationAdapter(adapter);
         AlphaInAnimationAdapter alphaAdapter2 = new AlphaInAnimationAdapter(adapter);
         ScaleInAnimationAdapter alphaAdapter = new ScaleInAnimationAdapter(alphaAdapter2);
-        alphaAdapter.setDuration(1000);
+        alphaAdapter.setDuration(1500);
         alphaAdapter.setInterpolator(new OvershootInterpolator());
         alphaAdapter.setFirstOnly(false);
         recyclerView.setAdapter(alphaAdapter);
@@ -331,6 +260,77 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+    }
+
+
+    private ArrayList<ImageModel> populateList(){
+
+        ArrayList<ImageModel> list = new ArrayList<>();
+
+        for(int i = 0; i < 4; i++){
+            ImageModel imageModel = new ImageModel();
+            imageModel.setImage_drawable(myImageList[i]);
+            list.add(imageModel);
+        }
+
+        return list;
+    }
+
+    private void init() {
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new SlidingImage_Adapter(MainActivity.this,imageModelArrayList));
+
+        CirclePageIndicator indicator = (CirclePageIndicator)
+                findViewById(R.id.indicator);
+
+        indicator.setViewPager(mPager);
+
+        final float density = getResources().getDisplayMetrics().density;
+
+//Set circle indicator radius
+        indicator.setRadius(5 * density);
+
+        NUM_PAGES =imageModelArrayList.size();
+
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == NUM_PAGES) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 3000, 3000);
+
+        // Pager listener over indicator
+        indicator.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPage = position;
+
+            }
+
+            @Override
+            public void onPageScrolled(int pos, float arg1, int arg2) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int pos) {
+
+            }
+        });
 
     }
 
